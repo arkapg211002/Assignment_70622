2965d0659250bbd9e8f90fcc73997d8c5e75c8f9
Arkapratim Ghosh <arkapratimghosh2002@gmail.com>
2022-06-04 10:30:03 +0530

    Updated cosine func added

--- Q3.c
+++ Q3.c
@@ -44,11 +44,32 @@ float sine(float n)
     while(i>=0)
     {
         t=pow(x,2*i+1)*pow(-1.0,i)/(float)factorial(2*i+1);
-        if(t<0.0000000001)
+        if((t>0 && t<0.0000000001)||(t<0 && t>-0.0000000001))
         {
             break;
         }
+        if(t==0){return 1.00000;break;}
         s=s+t;i++;
+    }
+    return s;
+}
+float cosine(float x)
+{
+    float s=0.0,t,n=((22/7)*x)/180;
+    int i=0,k=0;
+    while(k>=0)
+    {
+        t=pow(n,i)*pow(-1.0,k)/(float)factorial(i);
+        if((t>0 && t<0.0000000001)||(t<0 && t>-0.0000000001))
+        {
+            break;
+        }
+        if(t==0)
+        {
+            return 1.00000;
+            break;
+        }
+        s=s+t;k+=1;i+=2;
     }
     return s;
 }
@@ -59,7 +80,7 @@ void main()
     scanf("%f",&x);
     printf("The exponential series is %f\n",exponential(x));
     printf("The sine series is %f\n",sine(x));
-    printf("The cosine series is %f\n",pow(1-(sine(x)*sine(x)),0.5));
+    printf("The cosine series is %f\n",cosine(x));
 }
 
 
